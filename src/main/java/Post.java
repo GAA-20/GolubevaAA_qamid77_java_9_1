646d25f26847b26fb88bbce246e753d50a3b4d34
Alexandra <sana201193@gmail.com>
2024-06-12 15:42:55 +0700

    Add classes of task

---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+
+    public FormDate birthday; // поле для даты
+    public String name; // поле для имени
+    public String passport; // поле для паспорта
+    public String patronymic; // поле для отчетсва
+    public String phone; // поле для номера
+    public String surname; // поле для фамилии
+    public boolean subscription; // поле для подписки
+}
